3b6ef25809408a3369fa751f918f62c384337e73
ykchir <yecine.kchir@gmail.com>
2024-08-20 20:02:44 +0200

    Supprime le fichier IndicateDown.tsx

--- src/components/common/cards/IndicateDown.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import Indicate from './Indicate';
-
-const IndicateDown: React.FC<{ percent: number }> = ({ percent }) => {
-  return <Indicate percent={percent} color="#AE0000" direction="down" />;
-};
-
-export default IndicateDown;
